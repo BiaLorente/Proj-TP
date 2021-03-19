65ee427f01c4808bd6abe41005dfc342dc991bbb
Beatiz Lorente <beatriz.lorente@sitemercado.com.br>
2021-03-19 11:54:28 -0300

    troca para folder

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,6 @@ router.get('/equipe', function(req, res, next) {
 
 /* GET vitor_enrico page. */
 router.get('/vitor_enrico', function(req, res, next) {
-  res.render('vitor_enrico', { title: 'Vitor Enrico' });
+  res.render('vitor_enrico/vitor_enrico', { title: 'Vitor Enrico' });
 });
 module.exports = router;
